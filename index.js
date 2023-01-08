b28902dc54b3989f4c2efa02825e1a2b2e924ebd
MohitR1999 <mohitr1999@gmail.com>
2023-01-08 20:12:55 +0530

    Added the inital code for logging IP

---
+++ index.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 3000
+
+app.use(cors());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/test', (req, res) => {
+    const ip = req.ip;
+    console.log(`IP: ${ip}`);
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
